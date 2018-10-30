c20220d857e1bd3622ba116648b1f43d0dc59b45
bolgheroni <robertobolgheroni@gmail.com>
2018-10-30 21:33:50 -0200

    Starting development of the login page

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Contact from './views/Contact.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/contact',
       name: 'contact',
       component: Contact
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
